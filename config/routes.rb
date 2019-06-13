d30bc97ecec31d1c7e27efa49bb78a97fcb3872b
Gonenc <mete@angelraise.com>
2019-06-13 13:35:52 +0300

    added static page gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get '/index', :to => redirect('/index.html')
+  get "/index", to: static("index.html")
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
